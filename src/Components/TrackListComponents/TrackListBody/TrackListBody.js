4ad8b5415145467f3c0e8d04e5f8718505d70f39
Saymon85 <saymon_bg@hotmail.com>
2020-02-04 23:39:53 +0100

    add new component and samo minor refactor

--- src/Components/TrackListComponents/TrackListBody/TrackListBody.js
+++ src/Components/TrackListComponents/TrackListBody/TrackListBody.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import TrackListBodyItems from './TrackListBodyItems/TrackListBodyItems';
+import classes from './TrackListBody.css';
+
 
 const TrackListBody = (props) => {
     return (
         <div className='TrackList'>
             <div className='TrackListHeading'></div>
-            <div className='TrackListBody'></div>
+            <div className='TrackListBody'>
+                <TrackListBodyItems></TrackListBodyItems>
+            </div>
         </div>
     )
 }
+
+export default TrackListBody;
---
+++ src/Components/TrackListComponents/TrackListBody/TrackListBodyItems/TrackListBodyItems.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TrackListBodyItems = (props) => {
+    return <div></div>
+}
+
+export default TrackListBodyItems;
