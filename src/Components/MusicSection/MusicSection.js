74f7dddbe0d7dd99520de9fc48ca17a7fa8512bd
Saymon85 <saymon_bg@hotmail.com>
2020-03-08 19:42:18 +0100

    delete comments and console.log

--- src/Components/MusicSection/MusicSection.js
+++ src/Components/MusicSection/MusicSection.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
 import MusicSectionItem from './MusicSectionItem/MusicSectionItem';
 import classes from './MusicSection.css';
-//import carousel from '../../utilities/carousel';
-
 
 class MusicSection extends Component {
+    
     state = {
         offset: 0,
         counter: 0,
         maxed: false
     }
-    
-// TODO: pomeraj u desno radi kako treba u levo ne, proveri koliko se pomera u levo i kako
-// TODO: vidi da li moze da pomogne window.scrollX.... :)
 
     getDomElements(elementId, e) {
         const sliderContainer = document.querySelector(`#${elementId}`);
@@ -28,28 +24,18 @@ class MusicSection extends Component {
     }
 
     onLeftClick = (elementId, disableClass, e) => {
-/*         const sliderContainer = document.querySelector(`#${elementId}`);
-        const slider = sliderContainer.querySelector('ul');
-        const sliderItems = slider.querySelectorAll('li');
-        const sliderVisibleWidth = slider.offsetWidth;
-        const sectionHeading = e.target.closest('div');
-        const prevBtn = sectionHeading.querySelector('.Prev');
-        const nextBtn = sectionHeading.querySelector('.Next'); */
 
         const slider = this.getDomElements(elementId, e);
   
-
         if(slider.sliderItems.length % 4 !== 0 && this.state.maxed){
             if(!(this.state.counter - 1 === 0)){
                 const offsetCurrent = this.state.offset + slider.sliderVisibleWidth;
-                console.log(offsetCurrent);
                 slider.sliderList.style.transform = `translateX(${offsetCurrent}px)`;
                 slider.sliderList.style.transition = 'transform ease-in-out 0.75s';
                 slider.nextBtn.classList.remove(disableClass);
                 this.setState({offset: this.state.offset + slider.sliderVisibleWidth});
             }else{
                 const offsetCurrent = this.state.offset + ((slider.sliderVisibleWidth / 4) * (slider.sliderItems.length % 4));
-                console.log(offsetCurrent);
                 slider.sliderList.style.transform = `translateX(${offsetCurrent}px)`;
                 slider.sliderList.style.transition = 'transform ease-in-out 0.75s';
                 slider.prevBtn.classList.add(disableClass);
@@ -58,7 +44,6 @@ class MusicSection extends Component {
 
         }else{
             if(this.state.counter - 1 === 0){
-                console.log('na levo klik ako je ostao jedan pomeraj',this.state.offset);
                 const currentOffset = this.state.offset + slider.sliderVisibleWidth;
                 slider.sliderList.style.transform = `translateX(${currentOffset}px)`;
                 slider.sliderList.style.transition = 'transform ease-in-out 0.75s';
@@ -66,7 +51,6 @@ class MusicSection extends Component {
                 slider.prevBtn.classList.add(disableClass);
                 slider.nextBtn.classList.remove(disableClass);
             }else{
-                console.log('na levo ako ima vise od jednog pomeraja', this.state.offset);
                 const offsetCurrent = this.state.offset + slider.sliderVisibleWidth;
                 slider.sliderList.style.transform = `translateX(${offsetCurrent}px)`;
                 slider.sliderList.style.transition = 'transform ease-in-out 0.75s';
@@ -80,13 +64,6 @@ class MusicSection extends Component {
 
     onRightClick = (elementId, disableClass, e) => {
 
-/*         const sliderContainer = document.querySelector(`#${elementId}`);
-        const slider = sliderContainer.querySelector('ul');
-        const sliderItems = slider.querySelectorAll('li');
-        const sliderVisibleWidth = slider.offsetWidth;
-        const sectionHeading = e.target.closest('div');
-        const prevBtn = sectionHeading.querySelector('.Prev');
-        const nextBtn = sectionHeading.querySelector('.Next'); */
         const slider = this.getDomElements(elementId, e);
         const maxX = Math.floor(slider.sliderItems.length / 4);
 
@@ -97,18 +74,15 @@ class MusicSection extends Component {
             this.setState({offset: this.state.offset - ((slider.sliderVisibleWidth / 4) * (slider.sliderItems.length % 4))});
             slider.nextBtn.classList.add(disableClass);
             this.setState({maxed: !this.state.maxed});
-            console.log('counter', this.state.counter, 'offset', this.state.offset);
         }else{
             slider.prevBtn.classList.remove(disableClass);
             const offsetCurrent = this.state.offset - slider.sliderVisibleWidth;
             this.setState({offset: this.state.offset - slider.sliderVisibleWidth});
-            console.log('offset posle setState',this.state.offset, 'slider visible width', slider.sliderVisibleWidth);
             slider.sliderList.style.transform = `translateX(${offsetCurrent}px)`;
             slider.sliderList.style.transition = 'transform ease-in-out 0.75s';
             if(slider.sliderItems.length % 4 === 0 && (this.state.counter + 2 === maxX)){
                slider.nextBtn.classList.add(disableClass);
            }
-           console.log('counter', this.state.counter, 'offset', this.state.offset);  
        }
         this.setState({counter: this.state.counter + 1});
     }
